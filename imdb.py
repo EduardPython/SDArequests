cb3999625d3a7d379602750a60e9ff7886e16cab
petr.sevcik <petr.sevcik@kiwi.com>
2021-11-26 15:29:17 +0100

    regex added

--- imdb.py
+++ imdb.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 filmy_list = []
 r = requests.get("https://www.imdb.com/chart/top/")
@@ -8,10 +9,15 @@ for film in filmy:
     name = film.find("a").text
     year = film.find("span").text
     year = int(year.strip("()"))
-    result = (name, year)
+    director_and_actors_list = film.find("a").get("title").split(",")
+    director, *actors = director_and_actors_list
+    director = director[0:-6]
+    result = (name, year, director, actors)
     filmy_list.append(result)
 
+header = ["jméno", "rok vydání", "režisér", "hlavní herci"]
 sorted_films = sorted(filmy_list, key=lambda x: x[1], reverse=True)
-print(sorted_films)
-
-#getting director director = film.find("a").get("title")
+with open("imdb_top250.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+    writer.writerows(sorted_films)
